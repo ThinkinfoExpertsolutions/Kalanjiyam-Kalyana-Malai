54fce26039f792884d1d336924edf6ee2bf714e9
Subash <subashbanu827@gmail.com>
2024-12-04 18:59:08 +0530

    update cors

--- backend/index.js
+++ backend/index.js
@@ -6,34 +6,34 @@ import profileRouter from "./routes/profileRoute.js";
 import adminRouter from "./routes/adminRoute.js";
 import adminModel from "./model/adminModel.js";
 
-
 const app = express();
 const PORT = 5000;
 
 // DATABASE CONNECTION
-
 connectDB()
 
 // MIDDLEWARE
 
 app.use(express.json());
 
- app.use(cors());
+// CORS Configuration: Allow requests from the specific origin
+const corsOptions = {
+  origin: "https://www.kalanjiyamkalyanamalai.in",  // Allow only this domain
+  methods: ["GET", "POST", "OPTIONS","DELETE","PATCH"],  // Allow specific methods
+  allowedHeaders: ["Content-Type", "Authorization"]  // Allow specific headers
+};
 
+app.use(cors(corsOptions));  // Apply CORS middleware with options
 
-// API
-
-app.use("/api/user",userRouter);
-app.use("/api",profileRouter);
-app.use("/api/admin",adminRouter);
+// API ROUTES
+app.use("/api/user", userRouter);
+app.use("/api", profileRouter);
+app.use("/api/admin", adminRouter);
 app.get('/', (req, res) => {
     res.send('Hello World!');
-  })
-
-// SERVER  
-
-app.listen(PORT,()=>{
-    console.log(`Server Started At http://localhost:${PORT}`);
 });
 
-
+// SERVER
+app.listen(PORT, () => {
+    console.log(`Server Started At http://localhost:${PORT}`);
+});
