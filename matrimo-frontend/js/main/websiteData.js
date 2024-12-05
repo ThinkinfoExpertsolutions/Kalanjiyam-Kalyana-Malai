093479dbb9fa8fda137d9effc317bfc9bbcbc92a
Subash <subashbanu827@gmail.com>
2024-12-05 09:00:26 +0530

    check default vercel backend api

--- matrimo-frontend/js/main/websiteData.js
+++ matrimo-frontend/js/main/websiteData.js
@@ -3,7 +3,7 @@
 document.addEventListener("DOMContentLoaded",async()=>{
     showLoader()
     try {
-           const response = await fetch("https://api.kalanjiyamkalyanamalai.in/api/get-latest-profile", {
+           const response = await fetch("https://backend-alpha-flax.vercel.app/api/get-latest-profile", {
     method: "GET",
     headers: {
         "Content-Type": "application/json",
