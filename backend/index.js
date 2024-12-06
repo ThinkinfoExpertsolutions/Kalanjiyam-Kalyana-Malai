cedc824fc1e166111019d96dea90c24765fe94fb
ThinkinfoExpertsolutions <thinkinfoexpertsolutions@gmail.com>
2024-12-06 10:52:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,27 +13,27 @@ connectDB();
 
 // MIDDLEWARE
 app.use(express.json());
-
+app.use(cors());
 // Dynamic CORS Configuration
-const allowedOrigins = [
-  'https://www.kalanjiyamkalyanamalai.in',
-  'https://kalanjiyam-kalyana-malai-zhkl-5vbff4l4a.vercel.app'
-];
+// const allowedOrigins = [
+//   'https://www.kalanjiyamkalyanamalai.in',
+//   'https://kalanjiyam-kalyana-malai-zhkl-5vbff4l4a.vercel.app'
+// ];
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: 'GET,POST,PUT,DELETE,PATCH',
-  allowedHeaders: ['Content-Type', 'Authorization', 'token'],
-  credentials: true,
-};
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Handle preflight
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.includes(origin) || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: 'GET,POST,PUT,DELETE,PATCH',
+//   allowedHeaders: ['Content-Type', 'Authorization', 'token'],
+//   credentials: true,
+// };
+// app.use(cors(corsOptions));
+// app.options('*', cors(corsOptions)); // Handle preflight
 
 // ROUTES
 app.use("/api/user", userRouter);
